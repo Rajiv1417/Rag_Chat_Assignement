3ecefda494c91fca638d3af919e548a8f9387229
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-04-16 17:45:51 +0000

    feat: Update image extraction logic and add page_chunks parameter for improved markdown conversion

--- new_parser.py
+++ new_parser.py
@@ -1,7 +1,9 @@
 import pymupdf4llm
 
-md_text = pymupdf4llm.to_markdown("/workspaces/Rag_Chat_Assignement/Documens/pdfs/SC_2025_36 Introduction of LPT 1612g with 3.8 SGI TC CNG BS6 Ph2.pdf",
+md_text = pymupdf4llm.to_markdown("/workspaces/Rag_Chat_Assignement/Documens/pdfs/" \
+                                "SC_2025_36 Introduction of LPT 1612g with 3.8 SGI TC CNG BS6 Ph2.pdf",
                                 write_images=True,
+                                page_chunks = True,
                                 image_path="images_folder", # Directory where images will be saved
                                 image_format="png",         # Supported: "png", "jpg", etc.
                                 force_text=False,           # stop text extraction if images are present
